d7b1292b80930c99fdc535beb1470348f6e3d3f2
Architech User <architech@architech.(none)>
2015-02-06 10:37:23 +0100

    tmp

--- getyocto.sh
+++ getyocto.sh
@@ -17,6 +17,28 @@ function  internet_error()
     exit 1;
 }
 
+function  freescale_EULA()
+{
+    zenity --text-info \
+           --title="License" \
+           --filename=sources/meta-fsl-arm/EULA \
+           --checkbox="I read and accept the terms."
+
+    case $? in
+        0)
+            echo "Start installation!"
+            return 0
+	    ;;
+        1)
+            echo "Stop installation!"
+            return 1
+	    ;;
+        -1)
+            echo "An unexpected error has occurred."
+            return -1
+	    ;;
+    esac 
+}
 
 ARCHIVEFILE=${FILECHECK}
 MD5FILE="${ARCHIVEFILE}.md5"
@@ -34,7 +56,7 @@ fi
 
 cd ${YOCTO_DIRECTORY}
 
-wget --timeout=${WGET_TIMEOUT}  --ftp-user 'architech' --ftp-password 'architech'${FTPSERVER}${MD5FILE} 
+wget --timeout=${WGET_TIMEOUT}  --ftp-user 'architech' --ftp-password 'architech' ${FTPSERVER}${MD5FILE} 
 [ $? -eq 0 ] || { rm -f${MD5FILE}; internet_error; }
 
 if diff ${MD5FILE} ${MD5FILELOCAL} >/dev/null ; then
@@ -42,7 +64,7 @@ if diff ${MD5FILE} ${MD5FILELOCAL} >/dev/null ; then
   rm ${MD5FILE}  
 else
   wget --timeout=${WGET_TIMEOUT}  --ftp-user 'architech' --ftp-password 'architech' ${FTPSERVER}${ARCHIVEFILE}
-  [ $? -eq 0 ] || { rm -f ${ARCHIVEFILE}; internet_error; }
+   [ $? -eq 0 ] || { rm -f ${ARCHIVEFILE}; internet_error; }
   cp ../yocto_temp/sources.tar.bz2 .  
   wget --timeout=${WGET_TIMEOUT}  --ftp-user 'architech' --ftp-password 'architech' ${FTPSERVER}${VERSIONFILE}
   [ $? -eq 0 ] || { rm -f ${VERSIONFILE}; internet_error; }
@@ -72,6 +94,11 @@ if [ -d "build_opcapsolo" ]; then
     git pull origin master
     cd ..
 else
+    freescale_EULA
+    if [ $? -ne 0 ] ;then
+        echo "esco"
+        exit 1
+    fi
     git clone https://github.com/inkwaterman/build_opcapsolo.git
 fi
 
